f400d8ae5f6c7a20142632afb7116b22b34ba6cc
743953 <743953@elf7it218937.ACDM.DS.SAIT.CA>
2018-09-13 17:12:50 -0600

    Week 02 Lab Created and modified servlet, jsp, and xml. The application runs but with no functionalities yet.

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 743953
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form methoc="post" action="age">
+            Enter your age: <input type="text" name="age"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
